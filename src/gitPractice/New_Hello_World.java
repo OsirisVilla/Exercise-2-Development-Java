3018258e1065ce31a1f21bc0a07adca2532164b9
Osiris <osiris.villa.ca@gmail.com>
2019-04-10 12:46:07 -0500

    Adding another print line to see another commit in the pull request

--- src/gitPractice/New_Hello_World.java
+++ src/gitPractice/New_Hello_World.java
@@ -5,6 +5,7 @@ public class New_Hello_World {
 	public static void main(String[] args) {
 	
 		System.out.println("And I'm printing this to practice");
+		System.out.println("Im adding this to see my commit");
 
 	}
 
